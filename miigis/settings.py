16093046e842da92175db4073cfa3fd964ee7f9e
shizzgar <steeji.rat@gmail.com>
2019-02-14 22:41:49 +0300

    new fix

--- miigis/settings.py
+++ miigis/settings.py
@@ -140,7 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2',
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 # GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
